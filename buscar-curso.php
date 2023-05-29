82f563092cf5d5a4f4da220098044fa8c0e1a139
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-29 16:09:40 -0300

    realizando requisicao

---
+++ buscar-curso.php
@@ -0,0 +1,11 @@
+<?php
+
+use GuzzleHttp\Client;
+
+
+$client = new Client();
+
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $resposta->getBody();
+var_dump($resposta);
