496ba8f3f3f5e04fcde23bf204e82fc5e5d88647
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-30 08:52:43 -0300

    extraindo codigo para uma classe

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,22 +1,19 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'src/Buscador.php';
 
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeBursos\Buscador;
 
 
-$client = new Client(['verify' => false]);
-
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
+$client = new Client(['base_uri' => 'https://www.alura.com.br/' ,'verify' => false]);
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso . PHP_EOL;
 }
