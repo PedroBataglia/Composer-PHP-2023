d1a74049b03af0fd5e8595c9c3a30110903a0724
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-30 12:20:15 -0300

    adicionando classes e arquivos ao composer

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,11 +1,10 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'src/Buscador.php';
 
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
-use Alura\BuscadorDeBursos\Buscador;
+use Alura\BuscadorDeCursos\Buscador;
 
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/' ,'verify' => false]);
