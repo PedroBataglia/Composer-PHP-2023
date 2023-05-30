f81e168b31504b971f97b9caa9ea3ee6ec1cb1f4
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-05-30 07:23:07 -0300

    buscando cursos com bibliotecas

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,11 +1,22 @@
 <?php
 
+require 'vendor/autoload.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 
-$client = new Client();
+$client = new Client(['verify' => false]);
 
 $resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
 $html = $resposta->getBody();
-var_dump($resposta);
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
